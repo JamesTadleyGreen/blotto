158b8bd3947b02720aebdc51bacbb2a8a4d03ac0
James Green <JamesTadleyGreen@outlook.com>
2023-07-10 22:35:30 +0100

    Updating with custom assets

--- src/game.ts
+++ src/game.ts
@@ -1,41 +1,64 @@
 import * as Phaser from 'phaser';
+import { createAligned } from './parallax'
 
-export default class Demo extends Phaser.Scene
-{
-    constructor ()
-    {
+let keys = null
+let player = null
+
+export default class Demo extends Phaser.Scene {
+    constructor() {
         super('demo');
     }
 
-    preload ()
-    {
-        this.load.image('sky_cloud', 'assets/Background/sky_cloud.png');
-        this.load.image('sky', 'assets/Background/sky.png');
-        this.load.image('cloud', 'assets/Background/cloud.png');
-        this.load.image('mountain2', 'assets/Background/mountain2.png');
-        this.load.image('pine1', 'assets/Background/pine1.png');
-        this.load.image('pine2', 'assets/Background/pine2.png');
-        this.load.image('ground2', 'assets/Background/ground2.png');
-        this.load.tilemapTiledJSON('tilemap', 'assets/map.json')
+    preload() {
+        this.load.spritesheet('player', "/assets/Character/Player/_Run.png", { frameWidth: 120, frameHeight: 80 });
+        this.load.image('cloud', '/assets/Background/cloud.png')
+        this.load.image('mountain', '/assets/Background/mountain.png')
+        this.load.image('pine1', '/assets/Background/pine1.png')
+        this.load.image('pine2', '/assets/Background/pine2.png')
+        this.load.image('sky', '/assets/Background/sky.png')
     }
-    create ()
-    {
+    create() {
+        keys = this.input.keyboard.addKeys("W,S,D,A");
+        const width = this.scale.width
+        const height = this.scale.height
+        const totalWidth = width * 15
 
-        const map = this.make.tilemap({key: 'tilemap'})
-        var sky_cloud = map.addTilesetImage('sky_cloud.png', 'sky_cloud')
-        var mountain2 = map.addTilesetImage('mountain2.png', 'mountain2')
-        var pine1 = map.addTilesetImage('pine1.png', 'pine1')
-        var pine2 = map.addTilesetImage('pine2.png', 'pine2')
-        var ground2 = map.addTilesetImage('ground2.png', 'ground2')
-        map.createLayer('Tile Layer 1', [sky_cloud, ground2], 0,0)
-        map.createLayer('Tile Layer 2', [mountain2], 0,0)
-    }}
+        createAligned(this, totalWidth, 'sky', 0.15, 1.9)
+        createAligned(this, totalWidth, 'cloud', 0.15, 2.9)
+        createAligned(this, totalWidth, 'mountain', 0.2, 2.5)
+        createAligned(this, totalWidth, 'pine2', 0.35, 1.5)
+        createAligned(this, totalWidth, 'pine1', 0.45, 1.45)
 
+
+        this.anims.create({
+            key: 'walk',
+            frames: this.anims.generateFrameNumbers('player'),
+            frameRate: 16
+        });
+
+        player = this.add.sprite(250, 300, 'player').setScale(2);
+
+        player.play({ key: 'walk', repeat: -1 });
+
+        this.tweens.add({
+            targets: player,
+            x: 7500,
+            duration: 18800,
+            ease: 'Linear'
+        });
+
+        this.cameras.main.setBounds(0, 0, totalWidth, height);
+        this.cameras.main.startFollow(player, true, 0.08, 0);
+
+    }
+
+}
 const config = {
     type: Phaser.AUTO,
-    backgroundColor: '#125555',
-    width: 800,
-    height: 600,
+    width: 1000,
+    background: 0,
+    height: 400,
+    pixelArt: true,
     scene: Demo
 };
 
---
+++ src/parallax.js
@@ -0,0 +1,13 @@
+export function createAligned(scene, totalWidth, texture, scrollFactor, height) {
+    const w = scene.textures.get(texture).getSourceImage().width
+    const count = Math.ceil(totalWidth / w) * scrollFactor
+
+    let x = 0
+    for (let i = 0; i < count; ++i) {
+        const m = scene.add.image(x, scene.scale.height, texture)
+            .setOrigin(0, height)
+            .setScrollFactor(scrollFactor)
+
+        x += m.width
+    }
+}
